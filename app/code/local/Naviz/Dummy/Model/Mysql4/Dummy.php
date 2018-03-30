0bda386fa8d6069c3de1b5f6149f2e103a98a8fa
suvagiyaharshil <30555068+suvagiyaharshil@users.noreply.github.com>
2018-03-30 14:17:31 +0530

    Delete Dummy.php

--- app/code/local/Naviz/Dummy/Model/Mysql4/Dummy.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Naviz_Dummy_Model_Mysql4_Dummy extends Mage_Core_Model_Mysql4_Abstract
-{
-    protected function _construct()
-    {
-        $this->_init("dummy/dummy", "dummy_id");
-    }
-}
